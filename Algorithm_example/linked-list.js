97bc3ee2088f1d4d2d01b8d6fdec59d44990ea10
newExpand <magin0auto@gmail.com>
2023-01-13 22:10:38 +0900

    feat: [자료구조 및 알고리즘] linked-list 예제 정리

---
+++ Algorithm_example/linked-list.js
@@ -0,0 +1,60 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  find(value) {
+    let currNode = this.head;
+    while (currNode.value !== value) {
+      currNode = currNode.next;
+    }
+    return currNode;
+  }
+
+  append(newValue) {
+    let newNode = new Node(newValue);
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+  }
+
+  insert(node, newValue) {
+    const newNode = new Node(newValue);
+    newNode.next = node.next;
+    node.next = newNode;
+  }
+
+  remove(value) {
+    let preNode = this.head;
+    while (preNode.next.value !== value) {
+      preNode = preNode.next;
+    }
+
+    if (preNode !== null) {
+      preNode.next = preNode.next.next;
+    }
+  }
+
+  display() {
+    let currNode = this.head;
+    let displayString = "[";
+    while (currNode !== null) {
+      displayString += `${currNode.value}, `;
+      currNode = currNode.next;
+    }
+    displayString = displayString.substr(0, displayString.length - 2);
+    displayString += "]";
+  }
+}
