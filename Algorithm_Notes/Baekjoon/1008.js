54ee283891ba48027366f5ddec8498dd84c24a7f
newExpand <magin0auto@gmail.com>
2023-02-15 15:29:22 +0900

    feat : 현재 푼 파일들 정리 및 2507번 풀이 완료

--- Algorithm_Notes/Baekjoon/1008.js
+++
@@ -1,7 +0,0 @@
-const filePath = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
-const input = require("fs").readFileSync(filePath, "utf8").toString().split(" ");
-
-const num1 = input[0];
-const num2 = input[1];
-
-console.log(num1 / num2);
