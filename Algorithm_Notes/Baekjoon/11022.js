19343bf31ac6ae90dba3b89cf38cdb67c5c2438a
newExpand <magin0auto@gmail.com>
2023-02-15 16:39:50 +0900

    feat : 11022 문제풀이 완료

---
+++ Algorithm_Notes/Baekjoon/11022.js
@@ -0,0 +1,10 @@
+const filePath = "./example.txt";
+
+const input = require("fs").readFileSync(filePath, "utf8").toString().trim().split("\n");
+const len = input.shift();
+
+for (let i = 0; i < len; i++) {
+    let num = input[i].split(" ").map(Number);
+
+    console.log(`Case #${i + 1}: ${num[0]} + ${num[1]} = ${num[0] + num[1]}`);
+}
