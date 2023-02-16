e7201a04910f66f0d7922c13df1bd616062e10b5
newExpand <magin0auto@gmail.com>
2023-02-16 16:14:00 +0900

    feat : 2742 풀이 완료

---
+++ Algorithm_Notes/Baekjoon/2742.js
@@ -0,0 +1,11 @@
+const filePath = "./example.txt";
+
+const input = require("fs").readFileSync(filePath, "utf8").toString().trim();
+
+let result = "";
+for (let i = Number(input); i > 0; i--) {
+    result += i + "\n"
+}
+
+
+console.log(result);
