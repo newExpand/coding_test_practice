6dbfac2031e8cfdc01e6ebcdb5ac5153286e8fab
newExpand <magin0auto@gmail.com>
2023-02-19 17:15:52 +0900

    feat : 2438 풀이 완료

---
+++ Algorithm_Notes/Baekjoon/2/17/2438.js
@@ -0,0 +1,10 @@
+const filePath = "./example.txt";
+
+const input = require("fs").readFileSync(filePath, "utf8").toString().trim();
+let result = '';
+
+
+for (let i = 0; i < Number(input); i++) {
+    result += "*";
+    console.log(result)
+}
